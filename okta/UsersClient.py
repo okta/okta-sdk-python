4496b95629bfbff4a8c337c74b6e575093631095
jmelberg <jordan.melberg@okta.com>
2017-03-02 12:04:57 -0800

    :white_check_mark: Adds User and Groups API endpoint tests

--- okta/UsersClient.py
+++ okta/UsersClient.py
@@ -8,8 +8,13 @@ from okta.models.user.LoginCredentials import LoginCredentials
 
 
 class UsersClient(ApiClient):
-    def __init__(self, base_url, api_token):
-        ApiClient.__init__(self, base_url + '/api/v1/users', api_token)
+    def __init__(self, *args, **kwargs):
+        if 'base_url' in kwargs and 'api_token' in kwargs:
+            kwargs['base_url'] += '/api/v1/users'
+        else:
+            kwargs['base_url'] = args[0] + '/api/v1/users'
+            kwargs['api_token'] = args[1]
+        ApiClient.__init__(self, **kwargs)
 
     # CRUD
 
@@ -72,7 +77,7 @@ class UsersClient(ApiClient):
         :type activate: bool
         :rtype: User
         """
-        if activate is None:
+        if activate is False:
             response = ApiClient.post_path(self, '/', user)
         else:
             params = {
