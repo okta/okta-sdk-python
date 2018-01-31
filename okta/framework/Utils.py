49113295c96c54faebfdc1695a595c3aaf603ce4
Maximiliano Churichi <mchurichi@gmail.com>
2018-01-31 18:23:23 -0300

    :art: Fixes indent

--- okta/framework/Utils.py
+++ okta/framework/Utils.py
@@ -20,7 +20,7 @@ class Utils(object):
         if from_data is None or len(from_data) == 0:
             json_dump = {}
         elif isinstance(from_data, six.text_type) or isinstance(from_data, six.string_types):
-                json_dump = json.loads(from_data)
+            json_dump = json.loads(from_data)
         else:
             json_dump = from_data
 
