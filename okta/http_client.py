e704babbc222acf5590269c1fe74417072979532
Justin Abrokwah <justin.abrokwah@okta.com>
2020-05-29 13:57:13 -0400

    Impleneted base HTTPClient with aiohttp - Created tests also - Base request executor class for later implementation

--- okta/http_client.py
+++ okta/http_client.py
@@ -1,17 +1,53 @@
-from okta.in_memory_cache import InMemoryCache
-from okta.default_cache_middlware import DefaultCacheMiddleware
+import aiohttp
+import asyncio
 
 
 class HTTPClient:
-    def __init__(self, http_config={}):
-        self._default_headers = {}
-        self._requestExecutor = http_config.get("request_executor", None)
-        self._in_memory_cache = http_config.get(
-            "in_memory_cache") or InMemoryCache()
-        if http_config.get("cache_middleware") is not None:
-            self._cache_middleware = http_config.get("cache_middleware") or\
-                DefaultCacheMiddleware()
-        self._oauth = http_config.get("oauth")
+    """
+    This class is the basic HTTPClient for the Okta Client.
+    Custom HTTP clients should inherit from this class.
+    """
 
-    def add_header(self, new_header):
-        self._default_headers.update(new_header)
+    def __init__(self, http_config={}):
+        # Get headers from Request Executor
+        self._default_headers = http_config["headers"]
+        # Setup oauth if available
+        self._oauth = None if "oauth" not in http_config \
+            else http_config["oauth"]
+        # Create timeout for all HTTP requests
+        self._timeout = aiohttp.ClientTimeout(
+            total=http_config["requestTimeout"]
+        )
+
+    async def send_request(self, request):
+        """
+        This method fires HTTP requests
+
+        Arguments:
+            request {dict} -- This dictionary contains all information needed
+            for the request.
+            - HTTP method (as str)
+            - Headers (as dict)
+            - Request body (as dict)
+
+        Returns:
+            Tuple(aiohttp.RequestInfo, aiohttp.ClientResponse, JSON object)
+            -- A tuple containing the request and response of the HTTP call
+        """
+        try:
+            # Set headers
+            self._default_headers.update(request["headers"])
+            # Fire request
+            async with aiohttp.request(
+                method=request["method"],
+                url=request["url"],
+                headers=self._default_headers,
+                data=request["data"] or {},
+                timeout=self._timeout
+            ) as response:
+                return (response.request_info,
+                        response,
+                        await response.json(), None)
+        except (aiohttp.ClientError, asyncio.exceptions.TimeoutError) as error:
+            # Return error if arises
+            return (None, None, None, error)
--- okta/request_executor.py
+++ okta/request_executor.py
@@ -1,5 +1,7 @@
-import requests
-import json
+from okta.http_client import HTTPClient
+from okta.user_agent import UserAgent
+# import time
+# import json
 
 
 class RequestExecutor:
@@ -7,7 +9,10 @@ class RequestExecutor:
     This class handles all of the requests sent by the Okta Client.
     """
 
-    def __init__(self, config: dict = {}):
+    RETRY_COUNT_HEADER = 'X-Okta-Retry-Count'
+    RETRY_FOR_HEADER = 'X-Okta-Retry-For'
+
+    def __init__(self, config, cache, http_client=None):
         """
         Constructor for Request Executor object for Okta Client
 
@@ -19,26 +24,70 @@ class RequestExecutor:
         self._request_timeout = config.get('requestTimeout', 0)
         if self._request_timeout < 0:
             raise ValueError(
-                f"okta.client.rateLimit.requestTimeout provided as \
-                {self._request_timeout} but must be 0 (disabled) or \
-                greater than zero")
+                ("okta.client.rateLimit.requestTimeout provided as "
+                 f"{self._request_timeout} but must be 0 (disabled) or "
+                 "greater than zero"))
         self._max_retries = config.get('maxRetries', 2)
         if self._max_retries < 0:
             raise ValueError(
-                f"okta.client.rateLimit.maxRetries provided as \
-                {self._max_retries} but must be 0 (disabled) or \
-                greater than zero")
+                ("okta.client.rateLimit.maxRetries provided as "
+                 f"{self._max_retries} but must be 0 (disabled) or "
+                 "greater than zero"))
+        # Setup other fields
+        self._authorization_mode = config["client"]["authorizationMode"]
+        self._config = config
+        self._cache = cache
+        self._default_headers = {
+            'User-Agent': UserAgent(config["client"].get("userAgent", None))
+            .get_user_agent_string()
+        }
 
-        self._retryCountHeader = 'X-Okta-Retry-Count'
-        self._retryForHeader = 'X-Okta-Retry-For'
+        if config["client"]["authorizationMode"] == "SSWS":
+            self._default_headers['Authorization'] = (
+                "SSWS "
+                f"{self._config['client']['token']}"
+            )
+
+        self._http_client = HTTPClient({
+            'requestTimeout': self._request_timeout,
+            'headers': self._default_headers
+        })
 
     def create_request(self, method: str, url: str, body: dict = None,
                        headers: dict = None):
+        request = {
+            "method": method
+        }
 
-        pass
+        # Build request
+        headers = self._default_headers
+        url = self._config["client"]["orgUrl"] + url
 
-    def send_request(self, request):
-        # look into making this async
+        if self._authorization_mode == "PrivateKey":
+            # check if access token exists
+            if self._cache.contains("OKTA_ACCESS_TOKEN"):
+                access_token = self._cache.get("OKTA_ACCESS_TOKEN")
+                headers.update({"Authorization": access_token})
+            else:
+                pass
+
+        return request
+
+    def fire_request(self, request):
+        # Get start request time
+        # request_start_time = time.time()
+
+        url = request["url"]
+        method = request["method"]
+
+        if method.lower() == "GET".lower():
+            self._cache.delete(url)
+
+        # check if in cache
+        if not self._cache.contains(url):
+            # shoot request
+            req, res_details, res_json, error = self._http_client.send_request(
+                request)
         pass
 
     def retry_request(self, request):
