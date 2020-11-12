cd46492acc157c136b7ab65e69dd22b8ba39ce5e
hwallander86 <heatherwallander@c02xvajvjhd2.home>
2020-11-12 11:09:44 -0500

    Fix - Add data for Create User Request

--- okta/http_client.py
+++ okta/http_client.py
@@ -50,7 +50,6 @@ class HTTPClient:
                       'headers': self._default_headers,
                       'data': json.dumps(request['data'])}
             json_data = request.get('json')
-            
             # empty json param may cause issue, so include it if needed only
             # more details: https://github.com/okta/okta-sdk-python/issues/131
             if json_data:
