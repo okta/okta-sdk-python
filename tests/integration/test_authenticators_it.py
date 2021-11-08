173ccebf77487925f153eedba97db68fd6f0fdd0
Serhii Buniak <serhii.buniak@okta.com>
2021-11-08 16:30:44 +0200

    Add integration tests for Brand and Themes API, add new test for update_authenticator

--- tests/integration/test_authenticators_it.py
+++ tests/integration/test_authenticators_it.py
@@ -1,3 +1,4 @@
+import copy
 import pytest
 from tests.mocks import MockOktaClient
 import okta.models as models
@@ -48,3 +49,22 @@ class TestAuthenticatorsResource:
             assert err is None
             assert isinstance(resp, models.Authenticator)
             assert resp.status == 'INACTIVE'
+
+    @pytest.mark.vcr()
+    @pytest.mark.asyncio
+    async def test_update_authenticator(self, fs):
+        client = MockOktaClient(fs)
+        authenticators_list, _, err = await client.list_authenticators()
+        assert err is None
+
+        app_authenticator = [a for a in authenticators_list if a.type == 'phone'][0]
+        new_authenticator = copy.deepcopy(app_authenticator)
+        new_authenticator.settings.allowed_for = models.AllowedForEnum('RECOVERY')
+        try:
+            updated_authenticator, _, err = await client.update_authenticator(app_authenticator.id, new_authenticator)
+            assert err is None
+            assert isinstance(updated_authenticator, models.Authenticator)
+            assert updated_authenticator.settings.allowed_for == models.AllowedForEnum('RECOVERY')
+        finally:
+            updated_authenticator, _, err = await client.update_authenticator(app_authenticator.id, app_authenticator)
+            assert err is None
