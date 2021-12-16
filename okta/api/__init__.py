199ee88e20bf4087fde8ffd8cfcdf264a85b7b5d
Serhii Buniak <serhii.buniak@okta.com>
2021-12-16 16:45:37 +0200

    Re-generate using newest spec, 45 integration tests passed

--- okta/api/__init__.py
+++ okta/api/__init__.py
@@ -6,12 +6,12 @@ from __future__ import absolute_import
 from okta.api.application_api import Application
 from okta.api.authenticator_api import Authenticator
 from okta.api.authorization_server_api import AuthorizationServer
+from okta.api.brand_api import Brand
 from okta.api.captcha_api import CAPTCHA
 from okta.api.domain_api import Domain
 from okta.api.event_hook_api import EventHook
 from okta.api.feature_api import Feature
 from okta.api.group_api import Group
-from okta.api.group_schema_api import GroupSchema
 from okta.api.identity_provider_api import IdentityProvider
 from okta.api.inline_hook_api import InlineHook
 from okta.api.linked_object_api import LinkedObject
@@ -20,11 +20,11 @@ from okta.api.network_zone_api import NetworkZone
 from okta.api.org_api import Org
 from okta.api.policy_api import Policy
 from okta.api.profile_mapping_api import ProfileMapping
+from okta.api.schema_api import Schema
 from okta.api.session_api import Session
 from okta.api.template_api import Template
 from okta.api.threat_insight_api import ThreatInsight
 from okta.api.trusted_origin_api import TrustedOrigin
 from okta.api.user_api import User
 from okta.api.user_factor_api import UserFactor
-from okta.api.user_schema_api import UserSchema
 from okta.api.user_type_api import UserType
