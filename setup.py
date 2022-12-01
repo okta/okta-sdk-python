1bce2d50ed4d25ae2f078e73920da12dce103189
Justin Abrokwah <justin.abrokwah@okta.com>
2022-12-01 13:12:06 -0500

    Removing Python 3.6

--- setup.py
+++ setup.py
@@ -23,12 +23,11 @@ setup(
     long_description=open("LONG_DESCRIPTION.md").read(),
     test_suite="tests",
     packages=find_packages(exclude=("tests",)),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
