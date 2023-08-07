0906a75abbeef16148262b6e24954d3688b1f7af
Bryan Apellanes <bryan.apellanes@okta.com>
2023-08-07 10:12:51 -0500

    add scripts, update build

---
+++ run_integration_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export MOCK_TESTS=false
+export OKTA_CLIENT_ORGURL="https://dev-10592064.okta.com"
+export OKTA_CLIENT_TOKEN="00qULaxdPwhhK9k7NLuLEHmrFOwR1PjYdyGSsTdN7X"
+
+pytest tests/integration/
